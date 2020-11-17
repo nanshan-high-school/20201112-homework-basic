867ade9d7c86436a4b126f80ea46149db6cf7bf1
林柏崴 <65344622+wayne-1211@users.noreply.github.com>
2020-11-17 19:15:13 +0800

    Update Discord_login_system.cpp

--- 755446/Discord_login_system.cpp
+++ 755446/Discord_login_system.cpp
@@ -8,11 +8,10 @@ int main() {
                                   {"jack@gmail.com","654321"},
                                   {"good@yahoo.com.tw","987654"},
                                   {"happy@yahoo.com.tw","456789"},
-                                  {"sad@github.com","123789"}};   
-
+                                  {"sad@github.com","123789"}}; 
+    
     int num;
     bool status = false;
-    string wellcome;
     string account;
     string password;
     string name;
@@ -27,31 +26,19 @@ int main() {
     cin >> name;
 
     srand(time(0));
-    num = rand() %100;
+    num = rand() % 10;
 
-    if (num > 91) {
-        wellcome =  name +" 剛剛出現了！";
-    } else if (num > 82) {
-        wellcome = "歡迎，" + name + "。我們希望您帶個披薩來！";
-    } else if (num > 73) {
-        wellcome =  name +" 剛剛滑入了伺服器中。";
-    } else if (num > 64) {
-        wellcome =  name +" 剛剛著陸。"; 
-    } else if (num > 55) {
-        wellcome = "野生的 " + name + " 出現。";
-    } else if (num > 46) {
-        wellcome = "歡迎 " + name + "。打聲招呼吧。";
-    } else if (num > 37) {
-        wellcome = "很高興見到" + name;
-    } else if (num > 28) {
-        wellcome = name + " 在這裡。";
-    } else if (num > 19) {
-        wellcome = name + " 跳進了伺服器。";
-    } else if (num > 10) {
-        wellcome = "大家一起歡迎 " + name;
-    } else {
-        wellcome = "耶，" + name + "。成功了!";
-    }
+    string welcome[10] = {name +" 剛剛出現了！",
+                          "歡迎，" + name + "。我們希望您帶個披薩來！",
+                          name + " 剛剛滑入了伺服器中。",
+                          "野生的 " + name + " 出現。",
+                          name + " 跳進了伺服器。",
+                          name + " 剛剛滑入了伺服器中。",
+                          name + " 剛剛著陸。",
+                          "歡迎 " + name + "。打聲招呼吧。",
+                          "很高興見到" + name,
+                          "耶，" + name + "。成功了!"};
+
 
     for (int i = 0; i < 5; i++) {
         if (account_array[i][0] == account) {
@@ -65,7 +52,7 @@ int main() {
     }
     
     if (status == true) {
-        cout << wellcome;
+        cout << welcome[num];
     } else {
         cout << "你 是駭客嗎?(帳號或密碼錯誤!)" << "\n";
         return main();
@@ -85,3 +72,5 @@ int main() {
 大家憶起歡迎 []
 耶，[]。成功了!
 */
+
+
