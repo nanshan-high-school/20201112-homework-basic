e454f8485bbe7dc07cc5fbacd44f5dfae4a81f66
allison61014 <allison61014@gmail.com>
2020-11-20 02:12:58 +0800

    增加重複輸入

--- 750273/Discord 登入系統.cpp
+++ 750273/Discord 登入系統.cpp
@@ -16,24 +16,28 @@ int main() {
         {" 很高興見到您!"}
     };
 
-    string user, code;
-    cout << "帳號：";
-    cin >> user;
-    cout << "密碼：";
-    cin >> code;
+    int flag = 0;
 
-    int k;
-    srand(time(NULL));
-    k = rand() % 5;
-
-    for (int i = 0; i < 3; i++) {
-        if (user == account[i][0] && code == account[i][1]) {
-            cout << account[i][2] << messages[k];
-            break;
-        } else {
-            cout << "帳號密碼錯誤，請重新輸入。"; 
-            break;
+    do {
+        string user, code;
+        cout << "帳號：";
+        cin >> user;
+        cout << "密碼：";
+        cin >> code;
+    
+        int k;
+        srand(time(NULL));
+        k = rand() % 5;
+    
+        for (int i = 0; i < 3; i++) {
+            if (user == account[i][0] && code == account[i][1]) {
+                flag = 1;
+                cout << account[i][2] << messages[k];
+                break;
+            } else {
+                cout << "帳號密碼錯誤，請重新輸入。\n\n"; 
+                break;
+            }
         }
-    }
-
+    } while (flag != 1);
 }
